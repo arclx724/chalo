149dd6bbff4876ef37345b7b4bf103e46407ed9d
arclx724 <arclx724@gmail.com>
2026-04-10 07:09:38 +0530

    Refactor vars.py by removing unused variables
    Removed YT_COOKIES handling and various configuration options.

--- misskaty/vars.py
+++ misskaty/vars.py
@@ -2,7 +2,6 @@
 # * @date          2023-06-21 22:12:27
 # * @projectName   MissKatyPyro
 # * Copyright ©YasirPedia All rights reserved
-import requests
 import sys
 from logging import getLogger
 from os import environ
@@ -12,15 +11,6 @@ import dotenv
 LOGGER = getLogger("MissKaty")
 
 dotenv.load_dotenv("config.env", override=True)
-
-if YT_COOKIES := environ.get("YT_COOKIES"):
-    response = requests.get(YT_COOKIES)
-    if response.status_code == 200:
-        with open('cookies.txt', 'w') as file:
-            file.write(response.text)
-            LOGGER.info("Success download YT Cookies")
-    else:
-        LOGGER.info("Failed download YT Cookies")
 
 if API_ID := environ.get("API_ID", ""):
     API_ID = int(API_ID)
@@ -41,13 +31,12 @@ if not DATABASE_URI:
     sys.exit(1)
 if LOG_CHANNEL := environ.get("LOG_CHANNEL", ""):
     LOG_CHANNEL = int(LOG_CHANNEL)
-
 else:
     LOGGER.error("LOG_CHANNEL variable is missing! Exiting now")
     sys.exit(1)
+
 # Optional ENV
 LOG_GROUP_ID = environ.get("LOG_GROUP_ID")
-USER_SESSION = environ.get("USER_SESSION")
 DATABASE_NAME = environ.get("DATABASE_NAME", "MissKatyDB")
 TZ = environ.get("TZ", "Asia/Jakarta")
 PORT = environ.get("PORT", 80)
@@ -68,33 +57,3 @@ OPENAI_KEY = environ.get("OPENAI_KEY")
 GOOGLEAI_KEY = environ.get("GOOGLEAI_KEY")
 PAYDISINI_KEY = environ.get("PAYDISINI_KEY")
 PAYDISINI_CHANNEL_ID = environ.get("PAYDISINI_CHANNEL_ID", "17")
-
-## Config For AUtoForwarder
-# Forward From Chat ID
-FORWARD_FROM_CHAT_ID = list(
-    {
-        int(x)
-        for x in environ.get(
-            "FORWARD_FROM_CHAT_ID",
-            "-1001128045651 -1001455886928 -1001686184174",
-        ).split()
-    }
-)
-# Forward To Chat ID
-FORWARD_TO_CHAT_ID = list(
-    {int(x) for x in environ.get("FORWARD_TO_CHAT_ID", "-1001210537567").split()}
-)
-FORWARD_FILTERS = list(set(environ.get("FORWARD_FILTERS", "video document").split()))
-BLOCK_FILES_WITHOUT_EXTENSIONS = bool(
-    environ.get("BLOCK_FILES_WITHOUT_EXTENSIONS", True)
-)
-BLOCKED_EXTENSIONS = list(
-    set(
-        environ.get(
-            "BLOCKED_EXTENSIONS",
-            "html htm json txt php gif png ink torrent url nfo xml xhtml jpg",
-        ).split()
-    )
-)
-MINIMUM_FILE_SIZE = environ.get("MINIMUM_FILE_SIZE")
-CURRENCY_API = environ.get("CURRENCY_API")
